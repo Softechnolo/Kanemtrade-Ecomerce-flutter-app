8a6cd5f3bf3aa0d114dd0962c7391e931b6ce3ec
Mohammed Audu <meetsofset@gmail.com>
2022-03-25 11:15:44 +0100

    initial commit

---
+++ lib/other_config.dart
@@ -0,0 +1,6 @@
+
+class OtherConfig{
+  static const bool USE_PUSH_NOTIFICATION = false;
+  static const bool USE_GOOGLE_MAP = true;
+  static const String GOOGLE_MAP_API_KEY = "AIzaSyDJy-t9U9RjzW63y1uYUbcnF-4IF6xahU4";
+}
